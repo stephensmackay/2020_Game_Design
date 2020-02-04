31a85692b24fa745a240ea7d08ec621a4c5ee04f
StephenSMackay <10559104@my.uvu.edu>
2020-02-03 22:14:28 -0700

    3 Quarters Camera Set Up

---
+++ Fracture Veil/Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,17 @@
+﻿  
+using UnityEngine;
+
+public abstract class CharacterBase : ScriptableObject
+{
+    public CharacterController Controller { get; set; }
+
+    protected Vector3 Location;
+    protected Vector3 Orientation;
+    public float speed = 10f;
+    public float orientSpeed = .5f;
+    public float gravity = -9.81f;
+    public float jumpSpeed = 75f;
+    public abstract void Move();
+    protected Vector3 Position;
+
+}
---
+++ Fracture Veil/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CharacterMover : CharacterBase
+{
+    public float jumpSpeed =30f, gravity = 3f;
+        
+    public override void Move()
+    {
+        Location.Set(Input.GetAxis("Vertical")*speed,0,0);       
+        Orientation.y = Input.GetAxis("Horizontal") * orientSpeed;
+        Position.y -= gravity;
+
+        Controller.transform.Rotate(Orientation);
+        Location = Controller.transform.TransformDirection(Location);
+        
+        Controller.Move(Location * Time.deltaTime);
+
+        
+        //if(Input.GetButton("Jump"))
+       // {
+       //     Position.y = jumpSpeed;
+       // }
+       // else if (Controller.isGrounded)
+       // {
+       //     Position.y = 0;
+       // }
+    }
+}
