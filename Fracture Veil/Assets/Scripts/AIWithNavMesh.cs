375e3db5ec6b3b1e6e49d5215a7c5604ec54d262
StephenSMackay <10559104@my.uvu.edu>
2020-02-10 18:35:27 -0700

    NavMeshScript

---
+++ Fracture Veil/Assets/Scripts/AIWithNavMesh.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIWithNavMesh : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+
+    public Transform destination;
+
+    private void Start()
+    {
+        destination = transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+}
+
+
