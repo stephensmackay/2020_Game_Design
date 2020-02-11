60b240ab6c85300cef62c4d5145691014e017548
Stephen <10559104@my.uvu.edu>
2020-02-11 12:03:21 -0700

    NavMesh

---
+++ Fracture Veil/Assets/Scripts/FloatData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+ public float value;
+
+ public void modifyValue(float f)
+ {
+  value += f;
+ }
+ 
+}
