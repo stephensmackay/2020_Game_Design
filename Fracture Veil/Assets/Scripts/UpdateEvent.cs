16de3013f2a256bc24ea1b54cc48b357c0a0d10a
StephenSMackay <10559104@my.uvu.edu>
2020-02-12 20:57:33 -0700

    Vector3Data

---
+++ Fracture Veil/Assets/Scripts/UpdateEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class UpdateEvent : MonoBehaviour
+{
+
+    public UnityEvent updateEvent;
+    void Update()
+    {
+        updateEvent.Invoke();
+    }
+}
---
+++ Fracture Veil/Assets/Scripts/Vector3Data.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Vector3Data : ScriptableObject
+{
+    public Vector3 Vector3Obj;
+
+    public void UpdateVector3(Transform transformObj)
+    {
+        Vector3Obj = transformObj.position;
+    }
+}
