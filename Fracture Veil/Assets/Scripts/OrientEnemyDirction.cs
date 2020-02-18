88d93668a13c29a5cb888d537c942799ffa3ed9c
Stephen <10559104@my.uvu.edu>
2020-02-18 11:36:19 -0700

    Fix Enemy Attack

---
+++ Fracture Veil/Assets/Scripts/OrientEnemyDirction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class OrientEnemyDirction : ScriptableObject
+{
+
+    public GameObject originObj;
+
+    public void orientObj(Vector3Data targetObj)
+    {
+        originObj.transform.LookAt(targetObj.Vector3Obj);
+    }
+
+}
