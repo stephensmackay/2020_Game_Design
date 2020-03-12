49783a0230306d11f22cb770d988965b4f6db57f
Stephen <10559104@my.uvu.edu>
2020-03-12 09:36:10 -0600

    Enemy Health Script

---
+++ Fracture Veil/Assets/Scripts/EnemyHealthBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthBehavior : MonoBehaviour
+{
+    public int enemyHealth = 3;
+
+    void modifyHealth(int amount)
+    {
+        enemyHealth += amount;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        modifyHealth(-1);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
