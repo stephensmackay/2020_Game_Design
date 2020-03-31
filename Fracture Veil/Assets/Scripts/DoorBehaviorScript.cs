0a687195db3a8f0d96df861fdfcac87124908f8a
StephenSMackay <10559104@my.uvu.edu>
2020-03-31 11:08:02 -0600

    Door Working

--- Fracture Veil/Assets/Scripts/DoorBehaviorScript.cs
+++ Fracture Veil/Assets/Scripts/DoorBehaviorScript.cs
@@ -3,18 +3,34 @@
 public class DoorBehaviorScript : MonoBehaviour
 {
 
-    //public KeyCollectible keyList;
+    public bool canOpen = false;
 
     public Collections list;
     public void OpenDoor(Collectible obj)
     {
         if (list.collectablesList.Contains(obj))
         {
-            Debug.Log("Hi");
+            canOpen = true;
+            Debug.Log("can open");
+
         }
         else
         {
-            Debug.Log("No");
+            Debug.Log("You must obtain Key 1 Before proceeding");
+        }
+    }
+
+    public void DisableDoor(GameObject obj)
+    {
+        if (canOpen == true)
+        {
+            obj.SetActive(false);
+            Debug.Log("opening door");
+        }
+        else
+        {
+            Debug.Log("Door Locked");
+            return;
         }
     }
 }
