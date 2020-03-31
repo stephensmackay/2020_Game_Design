bee6b9d23469343c881e1940288109a8dba887be
StephenSMackay <10559104@my.uvu.edu>
2020-03-30 19:49:56 -0600

    Update Scene

--- Fracture Veil/Assets/Scripts/PushableBehavior.cs
+++ Fracture Veil/Assets/Scripts/PushableBehavior.cs
@@ -2,7 +2,7 @@
 
 public class PushableBehavior : MonoBehaviour
 {
-    public bool inPushRange = true;
+    public bool inPushRange = false;
     public bool beingPushed = false;
 
     public void PushOrStop(GameObject player)
@@ -10,16 +10,28 @@ public class PushableBehavior : MonoBehaviour
         if (inPushRange == true && beingPushed == false)
         {
             transform.parent = player.transform;
+            beingPushed = true;
         }
-        //else if (inPushRange == true && beingPushed == true)
-        //{
-        //    transform.parent = transform;
-        //}
+        else if (beingPushed == true)
+        {
+            transform.parent = null;
+            beingPushed = false;
+        }
         else
         {
             return;
         }
     }
-    
 
+    public void EnterRange()
+    {
+        inPushRange = true;
+    }
+
+    public void ExitRange()
+    {
+        inPushRange = false;
+    }
+    
+    
 }
