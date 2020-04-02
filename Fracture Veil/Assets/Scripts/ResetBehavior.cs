e610ab0018f5578838e0841bdc678c217b50b26b
StephenSMackay <10559104@my.uvu.edu>
2020-04-02 14:31:47 -0600

    fix bug

---
+++ Fracture Veil/Assets/Scripts/ResetBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ResetBehavior : MonoBehaviour
+{
+
+    public UnityEvent startEvent;
+    
+    void Start()
+    {
+        startEvent.Invoke();
+    }
+
+}
