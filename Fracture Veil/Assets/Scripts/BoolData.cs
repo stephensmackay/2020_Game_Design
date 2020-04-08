00468cea00ea05c894cf5345eeb917f866251d54
StephenSMackay <10559104@my.uvu.edu>
2020-04-08 17:21:17 -0600

    start switch behavior

--- Fracture Veil/Assets/Scripts/BoolData.cs
+++ Fracture Veil/Assets/Scripts/BoolData.cs
@@ -5,4 +5,46 @@ public class BoolData : ScriptableObject
 {
     public bool inPushRange = false;
     public bool beingPushed = false;
+    public bool oneActive, twoActive, threeActive, fourActive = false;
+    
+    public void SetOneActive()
+    {
+        oneActive = true;
+    }
+
+    public void SetOneFalse()
+    {
+        oneActive = false;
+    }
+    
+    public void SetTwoActive()
+    {
+        twoActive = true;
+        Debug.Log("activate");
+    }
+
+    public void SetTwoFalse()
+    {
+        twoActive = false;
+    }
+    
+    public void SetThreeActive()
+    {
+        threeActive = true;
+    }
+
+    public void SetThreeFalse()
+    {
+        threeActive = false;
+    }
+    
+    public void SetFourActive()
+    {
+        fourActive = true;
+    }
+
+    public void SetFourFalse()
+    {
+        fourActive = false;
+    }
 }
