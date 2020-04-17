20ad4672039bceee68a23b449e2dc9817a7ef403
StephenSMackay <10559104@my.uvu.edu>
2020-04-16 19:28:39 -0600

    update

---
+++ Fracture Veil/Assets/Scripts/EndGameBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class EndGameBehavior : MonoBehaviour
+{
+  public bool finalBoss;
+
+  public void FinishGame(GameObject obj)
+  {
+      if (finalBoss == true)
+      {
+          gameObject.SetActive(true);
+      }
+      else
+      {
+          return;
+      }
+  }
+
+  public void ChangeBool()
+  {
+    finalBoss = true;
+  }
+  
+}
