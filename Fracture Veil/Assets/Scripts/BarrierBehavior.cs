24be1113c89d0d099d07a394a8845df85f804fc5
StephenSMackay <10559104@my.uvu.edu>
2020-04-16 19:01:29 -0600

    update

---
+++ Fracture Veil/Assets/Scripts/BarrierBehavior.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class BarrierBehavior : MonoBehaviour
+{
+    public bool canOpen = false;
+
+    public bool conditionOne;
+
+    public bool conditionTwo;
+
+    public Collections list;
+
+    public GameObject textCanvas;
+
+    public Collectible obj1, obj2;
+    
+    public void CheckConditions()
+    {
+        if (list.collectablesList.Contains(obj1))
+        {
+            conditionOne = true;
+        }
+
+        if (list.collectablesList.Contains(obj2))
+        {
+            conditionTwo = true;
+        }
+
+       
+        if (conditionOne == true && conditionTwo == true)
+        {
+            canOpen = true;
+        }
+        else
+        {
+            return;
+        }
+      
+    }
+    
+    public void OpenDoor()
+    {
+        if (canOpen == true)
+        {
+            return;
+        }
+        else
+        {
+            textCanvas.SetActive(true);
+        }
+    }
+
+    public void DisableBarrier(GameObject obj)
+    {
+        if (canOpen == true)
+        {
+            obj.SetActive(false);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
